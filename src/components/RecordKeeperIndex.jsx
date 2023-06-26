7ca49217851746ce1dd3a667eeb6da4c6f698626
RAHUL <rahulmeetawa@gmail.com>
2023-06-26 21:48:06 +0530

    add A textfield

--- src/components/RecordKeeperIndex.jsx
+++ src/components/RecordKeeperIndex.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./Header";
+import TextField from "@mui/material/TextField";
 
 const RecordKeeperIndex = () => {
   return (
     <>
       <Header />
+      <TextField id="outlined-basic" label="Name" variant="outlined" />
     </>
   );
 };
