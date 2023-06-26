168d9c46f736154f0c423435c9174a702fcea97b
RAHUL <rahulmeetawa@gmail.com>
2023-06-26 21:52:44 +0530

    add A Button

--- src/components/RecordKeeperIndex.jsx
+++ src/components/RecordKeeperIndex.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import Header from "./Header";
 import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 
 const RecordKeeperIndex = () => {
   return (
     <>
       <Header />
-      <TextField id="outlined-basic" label="Name" variant="outlined" />
-      <TextField id="outlined-basic" label="Gmail" variant="outlined" />
+      <div>
+        <Stack direction="row" spacing={2}>
+          <TextField id="outlined-basic" label="Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Gmail" variant="outlined" />
+          <Button variant="contained" color="success">
+            ADD
+          </Button>
+        </Stack>
+      </div>
     </>
   );
 };
