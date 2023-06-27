2f6236151a26db4e634985fa916c43ddb6fdc8df
RAHUL <rahulmeetawa@gmail.com>
2023-06-27 12:01:34 +0530

    add a Content Components

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,7 +1,19 @@
 import React from "react";
-
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
 const Content = () => {
-  return <div></div>;
+  return (
+    <div className="form">
+      <Stack direction="row" spacing={2}>
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Gmail" variant="outlined" />
+        <Button variant="contained" color="success">
+          ADD
+        </Button>
+      </Stack>
+    </div>
+  );
 };
 
 export default Content;
