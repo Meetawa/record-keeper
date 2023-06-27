e37e9019bbff178b69faf820fc5dd83b16bbec7d
RAHUL <rahulmeetawa@gmail.com>
2023-06-27 12:02:06 +0530

    render a Content Components

--- src/components/RecordKeeperIndex.jsx
+++ src/components/RecordKeeperIndex.jsx
@@ -1,22 +1,12 @@
 import React from "react";
 import Header from "./Header";
-import TextField from "@mui/material/TextField";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
+import Content from "./Content";
 
 const RecordKeeperIndex = () => {
   return (
     <div className="App">
       <Header />
-      <div className="form">
-        <Stack direction="row" spacing={2}>
-          <TextField id="outlined-basic" label="Name" variant="outlined" />
-          <TextField id="outlined-basic" label="Gmail" variant="outlined" />
-          <Button variant="contained" color="success">
-            ADD
-          </Button>
-        </Stack>
-      </div>
+      <Content />
     </div>
   );
 };
