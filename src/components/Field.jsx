5c3c7cc1b89e83d5ffc9196e429e41a9ba73778e
RAHUL <rahulmeetawa@gmail.com>
2023-06-27 13:39:20 +0530

    add some css

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,19 +1,7 @@
 import React from "react";
 
-const Field = ({ data }) => {
-  return (
-    <>
-      {data.map((element, index) => {
-        return (
-          <div className="data-val" key={index}>
-            <h3>{element.name}</h3>
-            <h3>{element.email}</h3>
-            <h3>Remove</h3>
-          </div>
-        );
-      })}
-    </>
-  );
+const Field = () => {
+  return <div></div>;
 };
 
 export default Field;
