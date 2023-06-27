3466c74562ff8555d49030ad5488984947b8d477
RAHUL <rahulmeetawa@gmail.com>
2023-06-27 12:10:37 +0530

    render a footer Component

--- src/components/RecordKeeperIndex.jsx
+++ src/components/RecordKeeperIndex.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Content from "./Content";
+import Footer from "./Footer";
 
 const RecordKeeperIndex = () => {
   return (
     <div className="App">
       <Header />
       <Content />
-      <footer />
+      <Footer />
     </div>
   );
 };
