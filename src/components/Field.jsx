ab036e4a56d2a702805663076e46e2360b9ccd56
RAHUL <rahulmeetawa@gmail.com>
2023-06-27 13:47:29 +0530

    add a Field Component

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Field = ({ index, name, email }) => {
-  return <div></div>;
+  return (
+    <div className="data-val">
+      <h3>{name}</h3>
+      <h3>{email}</h3>
+      <h3>Remove</h3>
+    </div>
+  );
 };
 
 export default Field;
